c3a03af27be2e486e44b624bd486e00f91fc82d8
Your Name <you@example.com>
2022-10-09 14:18:47 +0200

    Test

---
+++ Test.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+
+using namespace std;
+
+int x,y,z;
+int main()
+{
+
+    cout<<"Wpisz ile uczniow jest w klasie:";
+    cin>>x;
+    cout<<"Ile masz cukierkow:";
+    cin>>y;
+    z = x/y;
+    cout<<"Kazdy dostanie po "<<z<<"cukierkow";
+
+    return 0;
+
+
+
+}
